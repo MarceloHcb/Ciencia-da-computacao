75c49b5666056f392e9e1cbb747819653b3d3831
MarceloHcb <marcelohcb@msn.com>
2023-07-04 07:14:44 -0300

    dia2

---
+++ algoritmos/dia1.py
@@ -0,0 +1,12 @@
+def number_substituition(numbers:list[int]):
+    for i in range(len(numbers)):
+       ...
+
+
+
+
+
+def test_number_substituition():
+    numbers_list = [2, 1, 2, 3, 0, 5, 1, 2, 3]
+    number_substituition(numbers_list)
+    assert numbers_list == [5,5,5,5,5,5,3,3,3]  
---
+++ dia2/course/numbers_sum.py
@@ -0,0 +1,5 @@
+my_number = 0
+while my_number < 42:
+    my_number += int(input("Enter a number: "))
+
+print("The sum of the numbers is greater than 42: ", my_number)
