0b9d540f1e992d1fa95cac45f166f3334f10d6c2
MarceloHcb <marcelohcb@msn.com>
2023-07-08 11:18:29 -0300

    test exe3

--- dia3/problemas/exe3.py
+++ dia3/problemas/exe3.py
@@ -17,8 +17,16 @@ def maiorinteiro(lista):
     tamanho = len(lista)
     return maiorinteiro_aux(lista, tamanho)
 
+def test_bigger_int():
+    assert bigger_int([5, 2, 8, 1, 9, 3]) == 9   
+    assert bigger_int([-5, -2, -8, -1, -9, -3]) == -1
+    assert bigger_int([-5, 2, 0, -1, 9, -3]) == 9
+    assert bigger_int([7]) == 7
+    assert bigger_int([3, 3, 3, 3]) == 3
+
 if __name__ == "__main__":
   my_list = [0,1,2,3,4,5,9,15,8,5]
   print(bigger_int(my_list))
 
 print(maiorinteiro([1, 21, 300, 4, 57]))
+
